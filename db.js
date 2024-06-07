e7d1dedefb9c28e2bf14158d4c87d1df32167b44
Rahul Kumar <132205464+RAHUl-CHAUDHARY01@users.noreply.github.com>
2024-06-07 19:37:46 +0530

    uri updated

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const mongoose= require('mongoose');
-const mongoURI= "mongodb://localhost:27017/inotebook"
+// const mongoURI= "mongodb://localhost:27017/inotebook"
+const mongoURI= "mongodb+srv://chaudharyrahul9315:L5MUD8mE6oRBiEfT@cluster0.vlqctrh.mongodb.net/"
+
 const connectToMongo=()=>{
   mongoose.connect(mongoURI, ()=>{
     console.log("connected to mongoose successgully");
